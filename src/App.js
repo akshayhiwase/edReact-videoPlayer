9b1eb6530e6ae2ef6cac363456e869d9d3aeaa0f
unknown <akshayhiwase96@gmail.com>
2020-02-20 01:57:54 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+import classes from './App.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className={classes.App}>
+        <VideoPlayer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Axios from 'axios'
+import classes from './VideoPlayer.module.css'
+class VideoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videoList: []
+        }
+    }
+    componentDidMount() {
+
+        Axios.get("https://5dfb78000301690014b8fbde.mockapi.io/playlist")
+            .then((response) => {
+                console.log(response.data)
+                this.setState({ videoList: [...response.data] })
+            })
+    }
+    render() {
+        const videoContainer = this.state.videoList.map((item) => {
+            return <div className={classes.videoCards} key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <h3>{item.title}</h3>
+            </div>
+        })
+        return (
+            <div className={classes.videoList}>
+                {videoContainer}
+
+            </div>
+        );
+    }
+}
+
+export default VideoPlayer;
